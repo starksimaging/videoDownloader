f917ac0cfedd2d4489b1aa8cf68f7b62bd989945
Jon <jonstarks6@gmail.com>
2026-05-13 10:09:36 -0400

    Initial Commit

---
+++ VideoDownloader/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VideoDownloader
+//
+//  Created by Jon Starks on 5/13/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
